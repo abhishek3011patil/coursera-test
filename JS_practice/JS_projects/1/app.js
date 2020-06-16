5f6a2b0740914a2318bc62ce8f463ff037b87310
unknown <abhishek3011patil@gmail.com>
2020-06-16 16:41:20 +0530

    practice of p1

--- JS_practice/JS_projects/1/app.js
+++ JS_practice/JS_projects/1/app.js
@@ -1,14 +1,14 @@
-const color = ["red", "green", "blue", "yellow"];
-const butt = document.getElementById("btn");
-const colort = document.querySelector(".color");
+const colors = ["red", "green", "pink", " blue", "yellow"];
+const color = document.querySelector(".color");
+const btn = document.getElementById("btn");
 
-butt.addEventListener("click", function(){
-    const RandomNumber=getRandomNo();
-    console.log(RandomNumber);
-    document.body.style.backgroundColor= color[RandomNumber];
-    colort.textContent =color[RandomNumber];
+btn.addEventListener("click", function(){
+    let random_no=changeColor();
+    document.body.style.backgroundColor = colors[random_no];
+         color.textContent = colors[random_no];
 });
 
-function getRandomNo(){
-   return Math.floor(Math.random()*color.length);
+
+function changeColor(){
+    return Math.floor(Math.random()*colors.length);
 }
