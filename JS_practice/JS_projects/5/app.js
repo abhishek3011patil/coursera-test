c2665a7e8dd5bac10c2b7aacc4bdcadce504d5bb
unknown <abhishek3011patil@gmail.com>
2020-06-16 23:21:01 +0530

    5 completed

---
+++ JS_practice/JS_projects/5/app.js
@@ -0,0 +1,15 @@
+const sidebar = document.querySelector(".sidebar");
+const toggle_btn = document.querySelector(".sidebar-toggle");
+const close_btn = document.querySelector(".close-btn");
+
+toggle_btn.addEventListener("click", function(){
+    
+   sidebar.classList.toggle("show-sidebar");
+});
+
+close_btn.addEventListener("click", function(){
+    sidebar.classList.remove("show-sidebar");
+
+});
+
+
