73cd1496258111424f5a29cd76b9fdd32bb705b6
unknown <abhishek3011patil@gmail.com>
2020-06-18 16:56:02 +0530

    7 completed

---
+++ JS_practice/JS_projects/7/app.js
@@ -0,0 +1,7 @@
+const btn = document.querySelectorAll(".question-btn");
+
+btn.forEach(function(btns){
+    btns.addEventListener("click", function(){
+        console.log(btns.parentElement);
+    });
+});
