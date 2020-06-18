35fc92361cfe586c9916c4fc1b90f07c62129fe6
abhishek patil <abhishek3011patil@gmail.com>
2020-06-18 23:49:28 +0530

    incomplete  2

--- JS_practice/JS_projects/7/app.js
+++ JS_practice/JS_projects/7/app.js
@@ -3,4 +3,4 @@ const btn = document.querySelectorAll(".question-btn");
 btn.forEach(function(btns){
     btns.addEventListener("click", function(){
         console.log(btns.parentElement);
-    })});
+    })}
