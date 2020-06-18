76cded51d28920cb5830a6942211e13a2644fe44
abhishek patil <abhishek3011patil@gmail.com>
2020-06-18 23:47:09 +0530

    in complete

--- JS_practice/JS_projects/7/app.js
+++ JS_practice/JS_projects/7/app.js
@@ -1,18 +1,6 @@
-const btns = document.querySelectorAll(".question-btn");
+const btn = document.querySelectorAll(".question-btn");
 
-btns.forEach(function(btn){
- btn.addEventListener("click", function(e){
-     console.log(e.currentTarget.parentElement.parentElement);
-     const que = e.currentTarget.parentElement.parentElement;
-     const ques = document.querySelectorAll(".question");
-     ques.forEach(function(items){
-         if(items !== que){
-             items.classList.remove("show-text")
-
-        }
-         console.log(items);
-     });
-     
-     que.classList.toggle("show-text");
- });
-});
+btn.forEach(function(btns){
+    btns.addEventListener("click", function(){
+        console.log(btns.parentElement);
+    })});
