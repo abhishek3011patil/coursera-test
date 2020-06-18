128e345fe3f2af923f7d93037bc1b18b1ccbbc2a
abhishek patil <abhishek3011patil@gmail.com>
2020-06-18 23:36:22 +0530

    2nd solution

--- JS_practice/JS_projects/7/app.js
+++ JS_practice/JS_projects/7/app.js
@@ -1,7 +1,18 @@
-const btn = document.querySelectorAll(".question-btn");
+const btns = document.querySelectorAll(".question-btn");
 
-btn.forEach(function(btns){
-    btns.addEventListener("click", function(){
-        console.log(btns.parentElement);
-    });
+btns.forEach(function(btn){
+ btn.addEventListener("click", function(e){
+     console.log(e.currentTarget.parentElement.parentElement);
+     const que = e.currentTarget.parentElement.parentElement;
+     const ques = document.querySelectorAll(".question");
+     ques.forEach(function(items){
+         if(items !== que){
+             items.classList.remove("show-text")
+
+        }
+         console.log(items);
+     });
+     
+     que.classList.toggle("show-text");
+ });
 });
