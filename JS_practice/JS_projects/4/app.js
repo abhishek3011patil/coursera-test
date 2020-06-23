c11e25b10757969d5bcc815b1f66091bec324328
abhishek patil <abhishek3011patil@gmail.com>
2020-06-23 23:18:30 +0530

    code fix

--- JS_practice/JS_projects/4/app.js
+++ JS_practice/JS_projects/4/app.js
@@ -5,9 +5,9 @@
 // toggle - toggles class
 
 const toggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
 
-
-toggle.addEventListener("click", function(){
+toggle.addEventListener("click", ()=>{
     console.log(links.classList);
     if(links.classList.contains("show-links")){
         links.classList.remove("show-links");
