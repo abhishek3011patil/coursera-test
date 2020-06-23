01164d52106339683b0e32f6cda0243d53bc9913
abhishek patil <abhishek3011patil@gmail.com>
2020-06-23 17:48:39 +0530

    comments added

--- JS_practice/JS_projects/11/app.js
+++ JS_practice/JS_projects/11/app.js
@@ -3,8 +3,11 @@ const abt = document.querySelector(".about");
 const cont = document.querySelectorAll(".content");
 
 abt.addEventListener("click", function(e){
+  // can be a good example of target vs currentTarget(does not work here)
+  // so id consists the value of element clicked
   const id = e.target.dataset.id;
   console.log(id);
+  //if statement to check wheter the data-id is present or not
   if(id){
     btns.forEach(function(btn){
       btn.classList.remove("active");
