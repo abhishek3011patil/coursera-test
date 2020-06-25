3f03c10db45d3dcb474e77309a45ed5027974468
abhishek patil <abhishek3011patil@gmail.com>
2020-06-25 16:21:39 +0530

    true or false color feature with reset second time added

--- JS_practice/ajax_tut/app.js
+++ JS_practice/ajax_tut/app.js
@@ -1,5 +1,5 @@
-const question = document.querySelector(".question")
- 
+const question = document.querySelector(".question");
+const btns = document.querySelectorAll(".btn");
 var quizAPI = "https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=boolean";
  
 apiRequest(quizAPI);
@@ -11,15 +11,38 @@ function apiRequest(url){
 quiz.onload = ()=>{
   var abc = JSON.parse(quiz.responseText);
   display(abc);
+   
 };
 quiz.send();
 
 }
+
+
 function display(abcjson){
     console.log(abcjson);
     question.innerHTML = abcjson.results[0].question;
+
+    btns.forEach((btn)=>{
+      btn.addEventListener("click" ,function(){
+        console.log( abcjson.results[0].correct_answer);
+        console.log( btn.dataset.id);
+        console.log( abcjson.results[0].correct_answer === btn.dataset.id);
+        btns.forEach((btn2)=>{
+          if(abcjson.results[0].correct_answer === btn.dataset.id){
+            btn2.classList.remove("butetfalse");
+         btn.classList.add("butettrue");
+        }
+         
+         else{
+         btn2.classList.remove("butettrue")
+         btn.classList.add("butetfalse");
+         }});
+
+        
+      });});
     
 }
+
 
 
 // quiz.open('GET', 'https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=boolean');
